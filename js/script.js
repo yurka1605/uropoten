e7a84a20342eb1b7d6f1b6bcf9701332107a70d5
Litvinov Yuriy <litvinov.yuriy1996@gmail.com>
2020-03-13 02:04:28 +0400

    fix(delete): delete no use scripts and styles

--- js/script.js
+++ js/script.js
@@ -1,35 +1,9 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-});
-
-$(window).on('scroll', function() {
-
-});
+// });
 
 /** ======================== User actions ========================== **/
-// scrool top
-$('.footer .banner__further span,.footer .further__arrow').on('click', function(e) {
-    e.preventDefault();
-    $('body, html').animate({ 
-        scrollTop: 0,
-    }, 300, 'swing');
-})
-// open menu
-// $('.header__menu_open').on('click', function() {
-//     $('.header__top').addClass('orange');
-//     $('.header__menu').addClass('active');
-//     if ($(window).width() < 1200) {
-//         $('.header-container').addClass('fullfield');
-//     } 
-// });
-// close menu
-// $('.close-btn').on('click', function() {
-//     $('.header__top').removeClass('orange');
-//     $('.header__menu').removeClass('active');
-//     if ($(window).width() < 1200) {
-//         $('.header-container').removeClass('fullfield');
-//     } 
-// });
+
 /** ======================== END:User actions ========================== **/
 
 
