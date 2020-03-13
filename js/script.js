7dd7e36c36f6aa8bfd63dd6b0f4f5de3af3d8579
yurka1605 <litvinov.yuriy1996@gmail.com>
2020-03-13 18:12:22 +0400

    feat(new): add sripts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-// $( document ).ready(function() {
-
-// });
+$(document).ready(function() {
+    $('#topPhone').mask('+7 (999) 999-9999');
+    $('#bottomPhone').mask('+7 (999) 999-9999');
+    $('banner__form').submit(function() {
+        var e = $(this);
+        return $.ajax({
+            type: 'POST',
+            url: 'mail.php',
+            data: e.serialize()
+        }).done((e) => {
+            console.log(e);
+        });
+    });
+});
 
 /** ======================== User actions ========================== **/
 
